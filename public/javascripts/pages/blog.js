3f46426afe780b283668b79cf6891a1bb79f681e
epitchi <thienvanlea1@gmail.com>
2022-07-24 22:44:10 +0700

    + Pull data post_blog

---
+++ public/javascripts/pages/blog.js
@@ -0,0 +1,30 @@
+import {
+  setDoc,
+  doc,
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.6.4/firebase-firestore.js";
+const db = getFirestore();
+const querySnapshot = await getDocs(collection(db, "blog_post"));
+let blog_post = document.getElementById("blog_post");
+let blogPostHTML = "";
+let styleOfArtical = "";
+querySnapshot.forEach((doc) => {
+  blogPostHTML += `
+      <article class="hentry">
+            <header class="entry-header">
+                <div class="entry-thumbnail">
+                    <a href="/blog-item">
+                    <img src="${doc.data().image}" alt="p1" />
+                    </a>
+                </div>
+                <h2 class="entry-title"><a href="/blog-item" rel="bookmark">${
+                  doc.data().title
+                }</a></h2>
+            </header>
+        </article>
+      `;
+  blog_post.innerHTML = blogPostHTML;
+});
