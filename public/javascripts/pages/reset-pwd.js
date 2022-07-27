74bc6dcd576b437bd1eed386fd20e0f66f772998
epitchi <thienvanlea1@gmail.com>
2022-07-27 08:14:29 +0700

    + Add reset password method 🥳

--- public/javascripts/pages/reset-pwd.js
+++ public/javascripts/pages/reset-pwd.js
@@ -1,12 +1,54 @@
-let sendBtn = document.getElementById("sendBtnElement");
+import {
+  getAuth,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/9.6.4/firebase-auth.js";
 
-sendBtn.addEventListener("click", function () {
-  handleValidate();
+let sendBtn = document.getElementById("sendBtnElement");
+const auth = getAuth();
+const newSwal = Swal.mixin({
+  heightAuto: false,
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+});
+
+sendBtn.addEventListener("click", async function () {
   const email = document.querySelector("input").value;
+
+  handleValidate();
   if (sendBtn.attributes["disabled"] == undefined && email.length > 0) {
-    console.log("send");
+    resetPassword(email);
   }
 });
+
+async function resetPassword(email) {
+  newSwal.fire({
+    title: "Sending",
+    text: "Just a moment . . .",
+    didOpen: () => {
+      Swal.showLoading();
+      sendPasswordResetEmail(auth, email)
+        .then(function () {
+          newSwal
+            .fire({
+              title: "Completed!",
+              text: "A password reset link was sent. Click the link in the email to create a new password.",
+              showConfirmButton: true,
+            })
+            .then(function () {
+              window.location.replace("/login");
+            });
+        })
+        .catch(function (error) {
+          var errorMessage = error.message;
+          newSwal.fire({
+            title: "Error!",
+            text: errorMessage,
+          });
+        });
+    },
+  });
+}
+
 function handleValidate() {
   const $textInputs = $("input");
   const $validTextInputs = $("input:valid");
