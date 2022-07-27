4b512b6a52cb6161a10e052a917865c4981ddfa0
epitchi <thienvanlea1@gmail.com>
2022-07-27 07:37:03 +0700

    + Add validate email send

--- public/javascripts/pages/reset-pwd.js
+++ public/javascripts/pages/reset-pwd.js
@@ -1,6 +1,19 @@
 let sendBtn = document.getElementById("sendBtnElement");
 
 sendBtn.addEventListener("click", function () {
+  handleValidate();
   const email = document.querySelector("input").value;
-  console.log(email);
+  if (sendBtn.attributes["disabled"] == undefined && email.length > 0) {
+    console.log("send");
+  }
 });
+function handleValidate() {
+  const $textInputs = $("input");
+  const $validTextInputs = $("input:valid");
+  const $submit = $("#sendBtnElement");
+  if ($textInputs.length === $validTextInputs.length) {
+    $submit.attr("disabled", null);
+  } else {
+    $submit.attr("disabled", "");
+  }
+}
